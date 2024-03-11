831e33a06b87a9b138c203a62c0c8a44b93fce4f
Ashurov Georgy <ashurov1308@mail.ru>
2024-03-11 17:21:42 +0300

    add all themes for button component

--- messenger-front/src/components/Button/Button.tsx
+++ messenger-front/src/components/Button/Button.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { Button, ButtonProps as MuiButtonProps } from "@mui/material";
 import styles from "./Button.module.scss";
+import cn from "classnames";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {};
+interface CustomButtonProps extends MuiButtonProps {
+  className?: string;
+  mode: "button__default" | "button__green" | "button__blue" | "button__dark";
+}
 
-const Button: React.FC<ButtonProps> = ({ ...props }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({
+  className,
+  mode,
+  ...props
+}) => {
   return (
-    <button {...props} className={styles.button}>
-      Отправить
-    </button>
+    <Button
+      {...props}
+      variant="contained"
+      className={cn(styles.button, className, styles[mode])}
+      color="primary"
+    >
+      Подключиться
+    </Button>
   );
 };
 
-export default Button;
+export default CustomButton;
